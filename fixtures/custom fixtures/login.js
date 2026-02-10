248bb43eee80307fd7bd7a00c0b6c920e9900bf3
tejas b m <tejas.trstech@gmail.com>
2026-02-10 10:29:44 +0530

    login

--- fixtures/custom fixtures/login.js
+++ fixtures/custom fixtures/login.js
@@ -1,15 +1,28 @@
-import { test as base } from '@playwright/test';
+import { test as base, expect } from '@playwright/test';
 
 export const test = base.extend({
   login: async ({ page }, use) => {
-
     await page.goto('http://202.83.16.221:9003/');
-    await page.locator("#email").fill("")
-     await page.locator("#password").fill("Tommy@123")
-     await page.getByRole("button",{name:" Login "}).click()
 
+    await page.locator('#email').fill(process.env.EMAIL);
+    await page.locator('#password').fill(process.env.PASSWORD);
 
-  }
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // wait till login completes
+    await page.waitForURL('**/dashboard');
+
+    // expose logged-in page
+    await use(page);
+  },
 });
 
-export { expect } from '@playwright/test';
+export { expect };
+
+//how to use it
+
+// import { test, expect } from '../fixtures/login.fixture';
+
+// test('user is logged in', async ({ login }) => {
+//   await expect(login.locator('text=Dashboard')).toBeVisible();
+// });
